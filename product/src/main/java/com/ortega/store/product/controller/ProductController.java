1f0e2b5d4ba5bb868519d36dadc91523cdeb97fb
Eddy <ortegachaveznsm@gmail.com>
2023-07-23 17:42:22 -0500

    Controller and Validators

--- product/src/main/java/com/ortega/store/product/controller/ProductController.java
+++ product/src/main/java/com/ortega/store/product/controller/ProductController.java
@@ -1,19 +1,24 @@
 package com.ortega.store.product.controller;
 
+import com.ortega.store.product.entity.Category;
 import com.ortega.store.product.entity.Product;
 import com.ortega.store.product.service.ProductService;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.validation.Valid;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -21,20 +26,86 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/list")
-    private ResponseEntity<List<Product>> getProductList(){
+    @GetMapping
+    public ResponseEntity<List<Product>> lstProduct(@RequestParam(name = "categoryId", required = false) Long categoryId){
         List<Product> products = new ArrayList<>();
-        products = productService.listAllProduct();
-        return ResponseEntity.ok(products);
 
+        if(categoryId == null){
+            products = productService.listAllProduct();
+        }else{
+            products = productService.findByCategory(Category.builder().id(categoryId).build());
+        }
+
+        if(products.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(products);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id){
+        Product product = productService.getProduct(id);
+        if(product == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
     @PostMapping("/save")
-    public ResponseEntity<Product> createProduct(@RequestBody Product product, BindingResult result){
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product, BindingResult result){
         if (result.hasErrors()){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "error");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, this.formatError(result));
         }
         Product productCreate =  productService.createProduct(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(productCreate);
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product){
+        product.setId(id);
+        Product productDB =  productService.updateProduct(product);
+        if (productDB == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(productDB);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable("id") Long id){
+        Product productDelete = productService.deleteProduct(id);
+        if (productDelete == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(productDelete);
+    }
+    @PutMapping (value = "/{id}/stock")
+    public ResponseEntity<Product> updateStockProduct(@PathVariable  Long id ,@RequestParam(name = "quantity", required = true) Double quantity){
+        Product product = productService.updateStock(id, quantity);
+        if (product == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    private String formatError(BindingResult result){
+        List<Map<String,String>> errors = result.getFieldErrors().stream()
+                .map(err->{
+                        Map<String,String> error = new HashMap<>();
+                        error.put(err.getField(), err.getDefaultMessage());
+                        return error;
+                }).collect(Collectors.toList());
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .code("01")
+                .messages(errors).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "";
+        try{
+            jsonString = mapper.writeValueAsString(errorMessage);
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return jsonString;
+    }
+
 }
--- product/src/main/java/com/ortega/store/product/entity/Product.java
+++ product/src/main/java/com/ortega/store/product/entity/Product.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.Date;
 
 @Data
@@ -15,17 +18,22 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+    @NotEmpty(message = "El nombre no debe ser vacio")
     private String name;
     private String description;
+    @PositiveOrZero(message = "El stock debe ser mayor que cero")
     private Double stock;
     private Double price;
     private String status;
+
     @Column(name = "create_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createAt;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn
     @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+    @NotNull(message = "Debe indicar una categoria")
     private Category category;
 
 
