116b4701d7973d983f152bf050da500075be300c
Eddy <ortegachaveznsm@gmail.com>
2023-07-24 01:54:10 -0500

    Invoice Services

---
+++ shopping/src/main/java/com/ortega/store/shopping/service/InvoiceService.java
@@ -0,0 +1,15 @@
+package com.ortega.store.shopping.service;
+
+import com.ortega.store.shopping.entity.Invoice;
+
+import java.util.List;
+
+public interface InvoiceService {
+    public List<Invoice> findInvoiceAll();
+
+    public Invoice createInvoice(Invoice invoice);
+    public Invoice updateInvoice(Invoice invoice);
+    public Invoice deleteInvoice(Invoice invoice);
+
+    public Invoice getInvoice(Long id);
+}
